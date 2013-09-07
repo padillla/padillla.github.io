490ef1628751dcc173b7cff83e96407270d80658
padillla <r.padilla.ruiz@gmail.com>
2013-09-07 12:32:07 -0600

    Recreated homepage

---
+++ javascripts/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Padilla
+ * Date: 9/7/13
+ * Time: 12:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
---
+++ javascripts/map.js
@@ -0,0 +1,9 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Rodolfo Padilla
+ * Date: 9/7/13
+ * Time: 12:30 PM
+ *
+ */
+
+
